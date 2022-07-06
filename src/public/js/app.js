6e152f64be86d3b0128125e1f147a53962ac7ac6
HDRP <hdrp0720@gmail.com>
2022-07-06 22:28:00 +0900

    Start projedct

---
+++ src/public/js/app.js
@@ -0,0 +1,166 @@
+const socket = io();
+
+const myFace = document.getElementById("myFace");
+const muteBtn = document.getElementById("mute");
+const cameraBtn = document.getElementById("camera");
+const camerasSelect = document.getElementById("cameras");
+
+let myStream;
+let muted = false;
+let cameraOff = false;
+let roomName;
+let myPeerConnection;
+
+muteBtn.addEventListener("click", handleMuteClick);
+cameraBtn.addEventListener("click", handleCameraClick);
+camerasSelect.addEventListener("input", handleCameraChange);
+
+async function getMedia(deviceId){
+  const initialConstrains = {
+    audio: true, 
+    video: {facingMode: "user"}
+  };
+  const cameraConstraints = {
+    audio: true,
+    video: {deviceId: {exact: deviceId}}
+  };
+  try{
+    myStream = await navigator.mediaDevices.getUserMedia(
+      deviceId? cameraConstraints : initialConstrains
+    );
+    myFace.srcObject = myStream;
+    if(!deviceId){
+      await getCameras();
+    }    
+  }
+  catch(e){
+    console.log(e);
+  };
+}
+async function getCameras(){
+  try{
+    const devices = await navigator.mediaDevices.enumerateDevices();    
+    const cameras = devices.filter(device => device.kind === "videoinput");
+    const currentCamera = myStream.getVideoTracks()[0];
+    cameras.forEach(camera => {
+      const option = document.createElement("option");
+      option.value = camera.deviceId;
+      option.innerText = camera.label;
+      if(currentCamera.label === camera.label){
+        option.selected = true;
+      }
+      camerasSelect.appendChild(option);
+    });
+  }
+  catch(e){
+    console.log(e);
+  }
+}
+function handleMuteClick(event){
+  myStream
+    .getAudioTracks()
+    .forEach((track) => {track.enabled = !track.enabled});
+
+  if(!muted){
+    muteBtn.innerText = "Unmute";
+    muted = true;
+  }
+  else{
+    muteBtn.innerText = "Mute";
+    muted = false;
+  }
+}
+function handleCameraClick(event){
+  myStream
+    .getVideoTracks()
+    .forEach((track) => {track.enabled = !track.enabled});
+    
+  if(cameraOff){
+    cameraBtn.innerText = "Turn Camera Off";
+    cameraOff = false;
+  }
+  else{
+    cameraBtn.innerText = "Turn Camera On";
+    cameraOff = true;
+  }
+}
+async function handleCameraChange(){
+  await getMedia(camerasSelect.value);
+  if(myPeerConnection){
+    const videoTrack = myStream.getVideoTracks()[0];
+    const videoSender = myPeerConnection
+      .getSenders()
+      .find((sender) => {sender.track.kind === "video"});
+    videoSender.replaceTrack(videoTrack);
+  }    
+}
+
+// Join a room (welcome Form)
+const welcome = document.getElementById("welcome");
+const call = document.getElementById("call");
+const welcomeForm = welcome.querySelector("form");
+
+call.hidden = true;
+
+welcomeForm.addEventListener("submit", handleWelcomeSubmit)
+
+async function handleWelcomeSubmit(event){
+  event.preventDefault();
+  const input = welcomeForm.querySelector("input");
+  await initCall();
+  socket.emit("join_room", input.value);
+  roomName = input.value;
+  input.value="";
+}
+async function initCall(){
+  welcome.hidden = true;
+  call.hidden = false;
+  await getMedia();
+  makeConnection();
+}
+
+// Socket Code
+socket.on("welcome", async () => {
+  const offer = await myPeerConnection.createOffer();
+  myPeerConnection.setLocalDescription(offer);
+  console.log("sent the offer");
+  socket.emit("offer", offer, roomName);
+})
+socket.on("offer", async(offer) => {
+  console.log("received the offer")
+  myPeerConnection.setRemoteDescription(offer);
+  const answer = await myPeerConnection.createAnswer();
+  myPeerConnection.setLocalDescription(answer);
+  socket.emit("answer", answer, roomName);
+  console.log("sent the answer");
+})
+socket.on("answer", answer => {
+  console.log("received the answer");
+  myPeerConnection.setRemoteDescription(answer);
+})
+socket.on("ice", ice => {
+  console.log("received candidate");
+  myPeerConnection.addIceCandidate(ice);
+})
+
+// WebRTC Code
+function makeConnection(){
+  myPeerConnection = new RTCPeerConnection();
+  myPeerConnection.addEventListener("icecandidate", handleIce);
+  myPeerConnection.addEventListener("addstream", handleAddStream);
+  myStream
+    .getTracks()
+    .forEach(track => myPeerConnection.addTrack(track, myStream));
+}
+function handleIce(data){
+  console.log("sent candidate");
+  socket.emit("ice", data.candidate, roomName);  
+}
+function handleAddStream(data){
+  // console.log("got a stream from my peer");
+  // console.log("Peer's Stream", data.stream);
+  // console.log("My stream", myStream);
+
+  const peerFace = document.getElementById("peerFace");
+  peerFace.srcObject = data.stream
+}
